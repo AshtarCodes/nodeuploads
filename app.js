5fb8e62d39f7ecb968f9955318c8e63f7925eb07
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-05-03 18:00:43 -0400

    port

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.post('/upload', (req, res) => {
   });
 });
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
